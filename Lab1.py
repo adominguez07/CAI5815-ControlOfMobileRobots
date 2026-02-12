d65ed107a90adb90eb79e43ef584eea17ca992bf
adominguez07 <adominguez21@usf.edu>
2026-02-11 19:25:45 -0500

    We moving now

--- Lab1.py
+++ Lab1.py
@@ -2,11 +2,13 @@ from HamBot.src.robot_systems.robot import HamBot
 import time
 
 def main():
-    bot = HamBot()
-    bot.left_wheel.set_speed(50)
-    bot.right_wheel.set_speed(50)
-    time.sleep(2)  # Run for 2 seconds
-    bot.stop()
+    bot = HamBot(lidar_enabled=False, camera_enabled=False)
     
+    bot.set_left_motor_speed(50)
+    bot.set_right_motor_speed(50)
+    time.sleep(2)  # Run for 2 seconds
+    bot.set_left_motor_speed(0)
+    bot.set_right_motor_speed(0)
+    return 0
 if __name__ == "__main__":
     main()
