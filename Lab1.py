4a9ef6943eda1fdc4f82f70eb4317cacf9c89cce
adominguez07 <adominguez21@usf.edu>
2026-02-11 20:10:20 -0500

    First actual run

--- Lab1.py
+++ Lab1.py
@@ -1,14 +1,56 @@
 from HamBot.src.robot_systems.robot import HamBot
 import time
 
+def rectangle(bot, length = 2, width = 4):
+    #width is the starting side
+    #robot start
+    #move half of the width
+    bot.set_right_motor_speed(50)
+    bot.set_left_motor_speed(50)
+    time.sleep(width/2)
+    #turn 90 degrees
+    bot.set_right_motor_speed(-50)
+    bot.set_left_motor_speed(50)
+    time.sleep(1)  # Adjust sleep time as needed
+    #move the length (east)
+    bot.set_right_motor_speed(75)
+    bot.set_left_motor_speed(75)
+    time.sleep(length)
+    #turn 90 degrees    bot.set_right_motor_speed(-50)
+    bot.set_left_motor_speed(50)
+    bot.set_right_motor_speed(-50)
+    #width(facing south)
+    bot.set_right_motor_speed(75)
+    bot.set_left_motor_speed(75)
+    time.sleep(width)
+    #turn 90 degrees
+    bot.set_right_motor_speed(-50)  
+    bot.set_left_motor_speed(50)
+    time.sleep(1)
+    #length(facing west)
+    bot.set_right_motor_speed(75)
+    bot.set_left_motor_speed(75)
+    time.sleep(length)
+    #turn 90 degrees
+    bot.set_right_motor_speed(-50)
+    bot.set_left_motor_speed(50)
+    time.sleep(1)
+    #width(facing north)
+    bot.set_right_motor_speed(50)
+    bot.set_left_motor_speed(50)
+    time.sleep(width/2)
+
+
+def CCW_circle(bot, radius = 1):
+    pass
+def CW_circle(bot, radius = 2):
+    pass
 def main():
     bot = HamBot(lidar_enabled=False, camera_enabled=False)
-    
-    bot.set_left_motor_speed(50)
-    bot.set_right_motor_speed(50)
-    time.sleep(2)  # Run for 2 seconds
-    bot.set_left_motor_speed(0)
-    bot.set_right_motor_speed(0)
+    rectangle(bot)
+    bot.disconnect_robot()
     return 0
+
+
 if __name__ == "__main__":
     main()
