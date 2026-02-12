0761379192a7ea51ed00f74e0310269293bacacd
adominguez07 <adominguez21@usf.edu>
2026-02-11 21:22:24 -0500

    Turn Function fix

--- Lab1.py
+++ Lab1.py
@@ -54,7 +54,7 @@ def turn(bot, angle, rpm = 50):
     V_right = angular_velocity_R * radius
     V_left = angular_velocity_L * radius
 
-    angular_robot = (V_right - V_left) / wheelbase
+    angular_robot = (V_left - V_right) / wheelbase
     time_to_turn = theta / angular_robot
 
     bot.set_right_motor_speed(-rpm)
